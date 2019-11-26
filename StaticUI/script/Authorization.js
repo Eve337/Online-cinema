92ca024ae2b6db0ff20d062c0f8e567ff84fcf55
Eve337 <vesunchik1945@gmail.com>
2019-11-26 22:55:07 +0300

    Added authorization script

--- StaticUI/script/Authorization.js
+++ StaticUI/script/Authorization.js
@@ -1,5 +1,64 @@
-const submitButton = function(form){
-    console.log(form.elements.login.value);
-    console.log(form.elements.password.value);
-    return false;
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'data/users.json', true);
+xhr.responseType = 'json';
+xhr.send();
+xhr.onload = function () {
+    let responseObj = xhr.response;
+    return responseObj;
+};
+
+function formCheckerSignIN(form) {
+    let loginValue = (form.elements.login.value);
+    let emailValue = (form.elements.password.value);
+    return userCheckSignIN(xhr.response, loginValue, emailValue);
+}
+
+function userCheckSignIN(obj, log, pass) {
+    let tagTrue = false;
+
+    for (let key in obj) {
+        if (obj[key]["email"] === log && obj[key]["password"] === pass) {
+            tagTrue = true;
+            localStorage.setItem('currentUser', JSON.stringify(obj[key]));
+        } else {
+            //handler invalid 
+        }
+    }
+    return tagTrue;
+}
+
+
+function userCheckSignUP(obj, log) { //Сhecking the existence of an account with the same mail
+    let tagTrue = true;
+    for (let key in obj) {
+        if (obj[key]["email"] === log) {
+            console.log("This email already using");
+            tagTrue = false;
+            //handler invalid
+        }     
+    }
+    return tagTrue;
+}
+
+function formCheckerSignUP(form) {
+    let newUserObj = {};
+    let loginValue = (form.elements.email.value);
+    let userCheckSignUp = userCheckSignUP(xhr.response, loginValue);
+
+    if(userCheckSignUp){
+        const objer = xhr.response;
+
+        newUserObj.id = Object.keys(objer).length + 1; 
+        newUserObj.email = form.elements.email.value;
+        newUserObj.password = form.elements.pass.value;
+        newUserObj.admin = false;
+        newUserObj.firstName = form.elements.fname.value;
+        newUserObj.lastName = form.elements.lname.value;
+        
+        localStorage.setItem('currentUser', JSON.stringify(newUserObj));
+        return true;
+        
+    } else {
+        return false;
+    }
 }
